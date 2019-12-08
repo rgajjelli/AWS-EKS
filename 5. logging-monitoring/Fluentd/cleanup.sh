e72550c7adf3da1d395b270eab7615b281f6aad7
Raj Gajjelli <gajjelli.r@gmail.com>
2019-12-08 20:57:40 +0000

    4.0 dashboard and iam

--- 5. logging-monitoring/Fluentd/cleanup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Edit this line, setting your EC2 worker node IAM role name
-ROLE_NAME=<worker node role>
-
-##-- DO NOT EDIT BELOW THIS LINE --##
-
-aws iam delete-role-policy --role-name $ROLE_NAME --policy-name Logs-Policy-For-Worker
-kubectl delete -f fluentd.yml
